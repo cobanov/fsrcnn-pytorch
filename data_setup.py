9c4b240c94e588938a9fa84add913655fe5fd412
Mert Cobanov <mertcobanov@gmail.com>
2022-11-30 03:02:52 +0300

    write custom dataset loader

--- data_setup.py
+++ data_setup.py
@@ -1,14 +1,21 @@
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
+from prep_data import MyDataset
+
 
 def create_dataloaders(
     train_dir: str,
     test_dir: str,
     transform: transforms.Compose,
+    transform_target: transforms.Compose,
     batch_size: int,
 ):
-    train_data = datasets.ImageFolder(train_dir, transform=transform)
-    test_data = datasets.ImageFolder(test_dir, transform=transform)
+    train_data = MyDataset(
+        train_dir, transform=transform, transform_target=transform_target
+    )
+    test_data = MyDataset(
+        test_dir, transform=transform, transform_target=transform_target
+    )
 
     train_dataloader = DataLoader(
         train_data,
--- prep_data.py
+++ prep_data.py
@@ -1,48 +1,26 @@
+from torch.utils.data import Dataset
+from torchvision import transforms
+from PIL import Image
 import os
-import random
-import shutil
-import argparse
-from pathlib import Path
 
 
-parser = argparse.ArgumentParser(
-    prog="Prep Data",
-    description="Splits images folder into train and test",
-    epilog="Text at the bottom of help",
-)
+class MyDataset(Dataset):
+    def __init__(self, image_paths, transform=None, transform_target=None):
+        self.image_paths = image_paths
+        self.transform = transform
+        self.transform_target = transform_target
 
-parser.add_argument("dataset_path")  # positional argument
-parser.add_argument("-s", "--train_size", type=float)  # option that takes a value
-parser.add_argument("-r", "--remove_source", type=bool, default=True)
+    def __getitem__(self, index):
+        x = Image.open(self.image_paths[index])
+        if self.transform_target:
+            y = self.transform_target(x)
+        else:
+            y = x.copy()
 
-args = parser.parse_args()
+        if self.transform:
+            x = self.transform(x)
 
+        return x, y
 
-def split_train_test(
-    dataset_path: str, train_size: int = 0.8, remove_source: bool = True
-) -> None:
-
-    try:
-        Path("dataset").mkdir(parents=True, exist_ok=True)
-        Path("dataset/train/class_0").mkdir(parents=True, exist_ok=True)
-        Path("dataset/test/class_0").mkdir(parents=True, exist_ok=True)
-
-    except:
-        pass
-
-    full_dataset = [os.path.join(dataset_path, i) for i in os.listdir(dataset_path)]
-    random.shuffle(full_dataset)
-    train_len = int(train_size * len(full_dataset))
-    train_paths, test_paths = full_dataset[:train_len], full_dataset[train_len:]
-
-    for path in train_paths:
-        shutil.move(path, "dataset/train/class_0")
-    for path in test_paths:
-        shutil.move(path, "dataset/test/class_0")
-
-    if remove_source:
-        os.removedirs(dataset_path)
-
-
-if __name__ == "__main__":
-    split_train_test(args.dataset_path, args.train_size, args.remove_source)
+    def __len__(self):
+        return len(self.image_paths)
